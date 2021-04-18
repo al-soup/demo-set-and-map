a1158225c813d1b5e1a1b7cd70b19185af9f1a66
al-soup <alex@kraeuchi.io>
2021-04-19 01:27:18 +0200

    Create first content

--- README.md
+++ README.md
@@ -1,3 +1,20 @@
-# typescript-urmfwc
+# Demo: Set & Map in JS
 
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-urmfwc)
+
+## Set
+
+## Set Functions
+- `clear()`
+- `add()`
+- `delete()`
+- `has()`
+
+### Takeaways
+- Focus on Unique values
+- Make your array consist of unique values: `[...new Set(arr)]`
+
+## Map
+- Remembers insertion key order
+
+### Takaways
--- index.ts
+++ index.ts
@@ -1,6 +1,38 @@
-// Import stylesheets
-import './style.css';
+import "./style.css";
 
-// Write TypeScript code!
-const appDiv: HTMLElement = document.getElementById('app');
-appDiv.innerHTML = `<h1>TypeScript Starter</h1>`;
+/**
+ * Setup
+ */
+const appDiv: HTMLElement = document.getElementById("app");
+
+/**
+ * Helpers
+ */
+const print = (v: any): void => {
+  const node = document.createElement("H1");
+  node.innerText = v;
+  appDiv.appendChild(node);
+};
+
+/**
+ * SET
+ */
+const mySet: Set<number> = new Set([1, 2, 3]);
+print("**** Sets ****");
+mySet.forEach(v => print(v));
+
+/**
+ * MAP
+ */
+print("**** Maps ****");
+
+const myMap: Map<string, string> = new Map([
+  ["key", "value"],
+  ["anotherKey", "anotherValue"]
+]);
+
+const a = myMap.get("anotherKey");
+print(a);
+myMap.forEach(v => print(v));
+
+print(myMap.size);

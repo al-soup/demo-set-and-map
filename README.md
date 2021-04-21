3bd6ab2144b045231df1f5e2d61f6a997005d96a
al-soup <alex@kraeuchi.io>
2021-04-21 09:22:49 +0200

    3 files modified

--- README.md
+++ README.md
@@ -3,6 +3,15 @@
 [Edit on StackBlitz ⚡️](https://stackblitz.com/edit/typescript-urmfwc)
 
 ## Set
+Sets are similar to arrays. Key difference: Sets don't contain duplicates and the insertion order is not remembered.
+
+Sets are iterable, so the `forEach` or `for-of` loop can be used on them.
+
+Delition and finding is more trivial and faster than in arrays (because there is no positioning/order).
+
+## Sets vs Arrays
+- Use sets when you want to make sure that every value is Unique
+
 
 ## Set Functions
 - `clear()`
--- index.ts
+++ index.ts
@@ -18,21 +18,41 @@ const print = (v: any): void => {
  * SET
  */
 const mySet: Set<number> = new Set([1, 2, 3]);
+
+// Add / remove
+mySet.add(4);
+mySet.delete(1);
+
 print("**** Sets ****");
-mySet.forEach(v => print(v));
+// mySet.forEach(v => print(v));
+
+// print(`Does set contain the number 2? - ${mySet.has(2)}`)
+
+// Convert set to array
+// const myArray: number[] = Array.from(mySet);
+const myArray: number[] = [...mySet]
+// myArray.forEach(v => print(v))
+
+// Create set from array
+const backToSet: Set<number> = new Set(myArray)
+
+// Remove duplicates from an array
+const noDuplicates = [...new Set([1,2, 3])]
+
+
 
 /**
  * MAP
  */
-print("**** Maps ****");
+// print("**** Maps ****");
 
-const myMap: Map<string, string> = new Map([
-  ["key", "value"],
-  ["anotherKey", "anotherValue"]
-]);
+// const myMap: Map<string, string> = new Map([
+//   ["key", "value"],
+//   ["anotherKey", "anotherValue"]
+// ]);
 
-const a = myMap.get("anotherKey");
-print(a);
-myMap.forEach(v => print(v));
+// const a = myMap.get("anotherKey");
+// print(a);
+// myMap.forEach(v => print(v));
 
-print(myMap.size);
+// print(myMap.size);
